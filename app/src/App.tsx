f0e5d814a6b0af27620400a04f30085cebce0c30
Emily Urdaneta <boss@Emilys-MacBook-Pro-2.local>
2023-10-30 18:14:54 -0600

    added the routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
-import Login from './components/Login/Login'
+import Login from './pages/Login/Login'
+import Home from './pages/Home/Home'
 import '../src/assets/Fonts.css'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
     return (
         <div>
-            <Login />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
         </div>
     )
 }
