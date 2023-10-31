e2c914d4e356701d2fdc915e3051439771283e2c
Emily Urdaneta <boss@Emilys-MacBook-Pro-2.local>
2023-10-30 19:46:28 -0600

    adding temp link in the homepage

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,8 +1,12 @@
+import { Button } from '../../components/Button/Button'
+import Layout from '../../components/Layout/Layout'
+import { Link } from 'react-router-dom'
+
 const Home = () => {
     return (
-        <div>
-            <p>HOME</p>
-        </div>
+        <Layout>
+            <Link to="/login">Login</Link>
+        </Layout>
     )
 }
 
