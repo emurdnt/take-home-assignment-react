cd173a78d2822de7879f36f09e9a3f66248388d3
Emily Urdaneta <boss@Emilys-MacBook-Pro-2.local>
2023-11-03 16:20:49 -0600

    cleaning up home file

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
-import { Button } from '../../components/Button/Button'
 import Layout from '../../components/Layout/Layout'
-
 import NavBar from '../../components/Nav/Nav'
 
 const Home = () => {
     return (
-        <Layout align="left">
+        <Layout backgroundColor="grey" align="left">
             <NavBar />
         </Layout>
     )
