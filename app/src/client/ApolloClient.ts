5536e890738fe4985d5d809a502578309012053e
Emily Urdaneta <boss@Emilys-MacBook-Pro-2.local>
2023-11-02 18:49:07 -0600

    adding handling for epxired sessions

--- app/src/client/ApolloClient.ts
+++ app/src/client/ApolloClient.ts
@@ -1,6 +1,84 @@
-import { ApolloClient, InMemoryCache, createHttpLink} from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink, from, fromPromise, gql} from "@apollo/client";
+import {onError} from "@apollo/client/link/error";
 import { setContext } from "@apollo/client/link/context";
 import { getTokens } from "../utilities/helpers";
+
+//need to update contet after refreshing tokens
+//how do I do that?
+let isRefreshing = false;
+let pendingRequests: any = [];
+
+const resolvePendingRequests = () => {
+  pendingRequests.map((callback: any) => callback());
+  pendingRequests = [];
+};
+
+const errorLink = onError(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors) {
+      for (let err of graphQLErrors) {
+        switch (err.extensions!.code) {
+          case 'UNAUTHENTICATED':
+            let forward$;
+
+            if (!isRefreshing) {
+              const tokens = getTokens();
+              isRefreshing = true;
+             
+              forward$ = fromPromise(
+                client
+                  .mutate({
+                    mutation: gql`
+                    mutation {
+                      refreshSession(accessToken: String!, refreshToken: String!) {
+                          accessToken
+                          refreshToken
+                          expiresAt
+                      }
+                  }
+                    `,
+                  variables:{
+                    accessToken: tokens.accessToken,
+                    refreshTokens: tokens.refreshToken,
+                  }
+                  })
+                  .then(({ data: { refreshToken } }) => {
+                    return true;
+                  })
+                  .then(() => {
+                    resolvePendingRequests();
+                    return true;
+                  })
+                  .catch(() => {
+                    pendingRequests = [];
+                    return false;
+                  })
+                  .finally(() => {
+                    isRefreshing = false;
+                  })
+              );
+            } else {
+              forward$ = fromPromise(
+                new Promise(resolve => {
+                  pendingRequests.push(() => resolve());
+                })
+              );
+            }
+
+            return forward$.flatMap(() => forward(operation));
+          default:
+            console.log(
+              `[GraphQL error]: Message: ${err.message}, Location: ${err.locations}, Path: ${err.path}`
+            );
+        }
+      }
+    }
+
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  }
+);
+
+
 
 
 const httpLink = createHttpLink({
@@ -22,7 +100,6 @@ const authLink = setContext((_, {headers}) =>{
 })
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: from([errorLink,authLink,httpLink]),
   cache: new InMemoryCache(),
-
 });
