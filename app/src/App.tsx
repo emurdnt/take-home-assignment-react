18054d5c64a9a6fbd88616440334d9bec53349d3
Emily Urdaneta <boss@Emilys-MacBook-Pro-2.local>
2023-11-02 18:48:48 -0600

    cleanup toapp.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,14 @@ import Login from './pages/Login/Login'
 import Home from './pages/Home/Home'
 import '../src/assets/Fonts.css'
 import { Routes, Route } from 'react-router-dom'
+import Products from './pages/Products/Products'
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/products" element={<Products />} />
         </Routes>
     )
 }
