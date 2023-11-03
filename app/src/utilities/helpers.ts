6fe922c8d67d27a5058a70a26da0dddf67697a2e
Emily Urdaneta <boss@Emilys-MacBook-Pro-2.local>
2023-11-03 16:19:57 -0600

    adding to utils

--- app/src/utilities/helpers.ts
+++ app/src/utilities/helpers.ts
@@ -4,10 +4,25 @@ export function saveTokens(tokens) {
   localStorage.setItem(TOKEN_KEY, JSON.stringify(tokens));
 }
 
-export function getTokens() {
+ export function getTokens() {
   return JSON.parse(localStorage.getItem(TOKEN_KEY));
-}
+ }
 
 export function deleteTokens() {
   localStorage.removeItem(TOKEN_KEY);
+}
+
+
+export const findInputError = (errors, name) => {
+  const filtered = Object.keys(errors)
+    .filter(key => key.includes(name))
+    .reduce((cur, key) => {
+      return Object.assign(cur, { error: errors[key] })
+    }, {})
+  return filtered
+}
+
+export const isFormInvalid = err => {
+  if (Object.keys(err).length > 0) return true
+  return false
 }
